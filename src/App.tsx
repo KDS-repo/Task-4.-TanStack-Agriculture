ac1714d996248515e3ebd293259f3b40d1c50f34
Dan <kohany.d@gmail.com>
2026-02-01 20:56:17 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Header } from "./Header"
+import { HomePage } from "./HomePage"
+
+export default function App() {
+    return (
+        <>
+            <Header/>
+            <HomePage/>
+        </>
+    )
+}
